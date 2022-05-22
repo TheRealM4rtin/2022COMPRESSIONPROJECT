1c67e2d5fa07dd5fb91ecfd64b199ed7f38bce12
Cece-root <76439709+Cece-root@users.noreply.github.com>
2022-05-22 18:40:00 +0200

    correction of the correction

--- rleCorrected.c
+++ rleCorrected.c
@@ -52,23 +52,16 @@ void RLEDecomp(char *compNAME, char *decompNAME){
     char c;
     int repetition = 0;
 
-    size_t pos2 = ftell(compfile);    // Current position
-    fseek(compfile, 0, SEEK_END);    // Go to end
-    size_t length2 = ftell(compfile); // read the position which is the size
-    fseek(compfile, pos2, SEEK_SET);
-
-    for (int i = 0; i < length2; i++){
-        c = getc(compfile);
+    while ((c = getc(compfile)) != EOF)
+    {
         if (c == '$'){
         }else{
             if (c >= '0' && c<= '9'){
                 if (repetition == 0)
                 {
                     repetition = c - '0';
-                    //printf("%d ", repetition);
                 }else{
                     repetition = 10*repetition + c - '0';
-                    //printf("%d \n", repetition);
                 }
             }else{
                 if (repetition == 0){
@@ -97,24 +90,19 @@ double compressionRate(char *fileNAME, char *compNAME){
     double I=0, F=0;
     double rate;
 
-    size_t pos = ftell(file);    // Current position
-    fseek(file, 0, SEEK_END);    // Go to end
-    size_t length = ftell(file); // read the position which is the size
-    fseek(file, pos, SEEK_SET);
-    I=length;
+    while ((c = getc(file)) != EOF)
+    {
+        I++;
+    }
+    printf("%f\n", I);
 
-    size_t pos2 = ftell(comp);    // Current position
-    fseek(comp, 0, SEEK_END);    // Go to end
-    size_t length2 = ftell(comp); // read the position which is the size
-    fseek(comp, pos2, SEEK_SET);
-    F=length2;
+    while ((c = getc(comp)) != EOF)
+    {
+        F++;
+    }
+    printf("%f\n", F);
 
     rate = (100-((F/I)*100));
-    
-    /*nom du fichier, type compression, compression rate
-    char temp[555];
-    sprintf(temp, "%s - RLE - Compression Rate : %f", fileNAME, rate);
-    fprintf(stat, "%s", temp);*/
 
     fclose(file);
     fclose(comp);

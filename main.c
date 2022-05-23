4de7533c5b035bd95f66f32860cafc7ac76018e7
Cece-root <76439709+Cece-root@users.noreply.github.com>
2022-05-23 21:13:50 +0200

    BR et RLE compression at the right place

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ const char *pic = ".bmp";
 
 //========================================================================================================//
 
-void BlankRemovalComp(char *fileNAME, char * baseCompNAME){
+void BlankRemovalComp(char *fileNAME, char * baseCompNAME, char * pathTemp){
     FILE * file;
     file = fopen(fileNAME,"r");
 
     FILE * comp;
-    char * compNAME;
-    sprintf(compNAME, "compBR%s", baseCompNAME);
+    char compNAME[500];
+    sprintf(compNAME, "%s/compBR%s", pathTemp, baseCompNAME);
     comp = fopen(compNAME,"w");
 
     char c;
@@ -49,6 +49,52 @@ void BlankRemovalComp(char *fileNAME, char * baseCompNAME){
     fclose(comp);
 }
 
+void RLEComp(char *fileNAME, char *baseCompNAME, char *pathTemp){
+    FILE * file;
+    file = fopen(fileNAME,"r");
+
+    FILE * comp;
+    char compNAME[500];
+    sprintf(compNAME, "%s/compRLE%s", pathTemp, baseCompNAME);
+    comp = fopen(compNAME,"w");
+
+    char c1, c2;
+    int repetition = 0;
+    c1 = getc(file);
+
+    while (!feof (file)){
+        c2 = getc(file);
+        if (c1 == c2)
+        {
+            repetition++;
+        }else{
+            if (repetition==0)
+            {
+                fprintf(comp, "%c", c1);
+            }else{
+                if (repetition >= 3){
+                    char temp3[500];
+                    repetition++;
+                    sprintf(temp3, "$%d%c", repetition, c1);
+                    fprintf(comp, "%s", temp3);
+                    repetition = 0;
+                }
+                if (repetition == 2){
+                    fprintf(comp, "%c%c%c", c1, c1, c1);
+                    repetition = 0;
+                }
+                if(repetition == 1){
+                    fprintf(comp, "%c%c", c1, c1);
+                    repetition = 0;
+                }
+            }
+        }
+        c1=c2;
+    }
+    fclose(file);
+    fclose(comp);
+}
+
 //========================================================================================================//
 
 struct NodePath
@@ -71,27 +117,32 @@ void PushPath(struct NodePath **head, char* data)
     *head = temp;
 }
 
+//========================================================================================================//
+
 void comparison(struct NodePath *head, char * pathHeader, char * nameFile){
     FILE * header;
-    header = fopen(pathHeader, "a");
+    char realPathHeader[555];
+    sprintf(realPathHeader, "%s/header.txt", pathHeader);
+    header = fopen(realPathHeader, "a");
 
     if( header == NULL ) {
-        printf("Couldn't open %s\n", pathHeader);
+        printf("Couldn't open %s\n", realPathHeader);
         exit(1);
-    }
+    }else    printf("Could open %s\n",realPathHeader);
 
     for (; head; head = head->next){
-        char *comp= (head->c);
-        char * test1 = strstr(comp, text);
-        char * test2 = strstr(comp, pic);
+        char *compar= (head->c);
+        char * test1 = strstr(compar, text);
+        char * test2 = strstr(compar, pic);
 
         if (test1 || test2){
-            printf("This is a file to compress : %s\n", comp);
-            fprintf(header, "%s\n", comp);
-            BlankRemovalComp(comp, nameFile);
+            printf("This is a file to compress : %s\n", compar);
+            fprintf(header, "%s\n", compar);
+            BlankRemovalComp(compar, nameFile, pathHeader);
+            RLEComp(compar, nameFile, pathHeader);
         }
         else
-            printf("This is not a file  to compress : %s\n", comp);
+            printf("This is not a file  to compress : %s\n", compar);
     }
     fclose(header);
 }
@@ -105,8 +156,6 @@ void traversal(char *originPath, char * PathHeader)
     DIR *directory = opendir(originPath);
 
     struct NodePath *head;
-
-    
 
     if (!directory)
         return;
@@ -129,7 +178,6 @@ void traversal(char *originPath, char * PathHeader)
             traversal(path, PathHeader);
         }
     }
-
     closedir(directory);
 }
 
@@ -145,18 +193,23 @@ int main()
     printf("Please enter the session's user name : ");
     scanf("%s", username);
 
-    char PathHeader[555];
-    sprintf(PathHeader, "/home/%s/Desktop/FCtemporary/header.txt", username); //need just /home/%s/Desktop/FCtemporary
+    char PathHeader[555]="";
+    sprintf(PathHeader, "/home/%s/Desktop/FCtemporary/header.txt", username);
     //clean the header file text from last execution
     fclose(fopen(PathHeader, "w"));
 
+    char fileTemp[555]="";
+    sprintf(fileTemp, "/home/%s/Desktop/FCtemporary", username);
+
+    //____________________________________________________
+
     //string to put the path of the user
-    char path[50];
-    printf("\nPlease enter path the folder : ");
+    char path[50]="";
+    printf("Please enter path the folder : ");
     scanf("%s", path);
 
     //fonction to get the .txt etc in the folder chosen
-    traversal(path, PathHeader);
+    traversal(path, fileTemp);
 
     return 0;
 }

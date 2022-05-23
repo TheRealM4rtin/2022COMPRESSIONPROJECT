8271c1edba1b1ac00f02eab56cf439aaf82d34e3
Martin <dinamyx.ytb@gmail.com>
2022-05-23 22:35:57 +0200

    elp

---
+++ huffmanDEAD.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list {
+    int freq;
+    struct list *next;
+    struct tree * tree;
+}* list;
+
+typedef struct tree{
+    char data;
+    struct tree *left, *right;
+} *tree;
+
+list ConstructList(int frequence, list next, tree tree){
+    list new = (list)malloc(sizeof(struct list));
+    new->freq = frequence;
+    new->next = next;
+    new->tree = tree;
+    return new;
+}
+
+tree ConstructTree(char data, tree left, tree right){
+    tree new = (tree)malloc(sizeof(struct tree));
+    new->data = data;
+    new->left = left;
+    new->right = right;
+    return new;
+}
+/*
+void printtree(tree B){
+    if (B!=NULL)
+    {
+        if (B->left != NULL || B->right !=NULL)
+        {
+            printf("1 ");
+        }
+        printtree(B->left);
+        printtree(B->right);
+        if (B->left == NULL && B->right == NULL)
+        {
+            printf("0%c ", B->data);
+        }
+    }else{}
+}
+*/
+void print(struct list *head) {
+    for (; head; head = head->next)
+        printf(" - frequence of %c is %d\n", head->tree->data, head->freq);
+}
+
+void SortedInsert(struct list** headRef, struct list* newNode) {
+    list current = *headRef;
+    // Special case for the head end
+    if (*headRef == NULL || (*headRef)->freq >= newNode->freq) {
+        newNode->next = *headRef;
+        *headRef = newNode;
+    }
+    else {
+        // Locate the node before the point of insertion
+        while (current->next!=NULL && current->next->freq<newNode->freq) {
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+}
+
+void InsertSort(struct list** headRef) {
+    list result = NULL, current = *headRef, next;
+    while (current!=NULL) {
+        next = current->next;
+        SortedInsert(&result, current);
+        current = next;
+    }
+    *headRef = result;
+}
+
+void add_freq(struct list *head, tree tree, int newfreq) {
+    for(; head; head = head->next)
+        if (head->tree->data == tree->data ) head->freq += newfreq;
+}
+
+int is_in(tree tree, struct list *list) {
+    for (; list; list = list    ->next)
+        if (list->tree->data == tree->data) return 1;
+    return 0; // is not in
+}
+
+tree CodingTree(struct list **head){
+    list * current = head ;
+    tree final = NULL;
+
+    while( (*current)->next) {
+        list node_1 = *head;
+        list node_2 = (*head)->next;
+        printf("---------------------\n");
+
+
+        tree tree_1 = ConstructTree(node_1->tree->data, NULL, NULL);
+        tree tree_2 = ConstructTree(node_2->tree->data, NULL, NULL);
+        int sum = (node_1->freq) + (node_2->freq);
+
+        tree treeresult = ConstructTree(' ', tree_1, tree_2);
+        
+        /* Push in the head */ //
+        *head = ConstructList(sum, (*head)->next->next, treeresult);
+
+    
+
+        //printf("Total FREQ : %d", list_tree->freq);
+        
+        InsertSort(head);
+        printf("\n");
+        print(*head);
+
+        current = &((*current)->next);
+    }
+    
+    return final;
+}
+
+
+void occurency(char *fileNAME)
+{
+    FILE *file;
+    file = fopen(fileNAME, "r");
+
+    list h = NULL;
+    list *a = &h;
+    tree T = NULL;
+    tree temp = NULL;
+    char c;
+
+    while ((c = getc(file)) != EOF)
+    {
+        temp->data = c;
+        if (is_in(temp, h) == 1)
+        { // If in the dictionnary
+            printf("%c is in the dico\n", temp->data);
+            T = ConstructTree(temp->data, NULL, NULL);
+            add_freq(h, T, 1);
+        }
+        if (is_in(temp, h) == 0)
+        { // If not in the dictionnary
+            printf("%c is Not in the dico\n", temp->data);
+            T = ConstructTree(temp->data, NULL, NULL);
+            *a = ConstructList(1, NULL, T);
+            a = &((*a)->next);
+        }
+    }
+    /*
+    InsertSort(&h);
+    printf("\n####TABLE OF OCCURENCES####\n");
+    print(h);
+    printf("###########################\n\n");
+     
+   
+    CodingTree(&h);
+    
+*/
+
+    fclose(file);
+}
+
+int main()
+{
+    char *test = "test.txt";
+    occurency(test);
+
+}
